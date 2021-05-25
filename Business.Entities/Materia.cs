393c33a3bae443a6fefe2661e1a02803bfaae8dc
Ivan Biscaldi <ivanbisc12@gmail.com>
2021-05-25 13:28:23 -0300

    +Entidades de negocio

---
+++ Business.Entities/Materia.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Materia: BusinessEntity
+    {
+        private int _IDPlan,_HsSemanales, _HsTotales;
+        private string _Descripcion;
+
+        public int IDPlan
+        {
+            get { return this._IDPlan; }
+            set { this._IDPlan = value; }
+        }
+
+        public int HsSemanales
+        {
+            get { return this._HsSemanales; }
+            set { this._HsSemanales = value; }
+        }
+
+        public int HsTotales
+        {
+            get { return this._HsTotales; }
+            set { this._HsTotales = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return this._Descripcion; }
+            set { this._Descripcion = value; }
+        }
+    }
+}
---
+++ Business.Entities/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Persona: BusinessEntity
+    {
+        private string _Apellido, _Direccion, _Email;
+        private DateTime _FechaNacimiento;
+        private int _IDPlan, _Legajo;
+        private string _Nombre, _Telefono;
+        //private TipoPersona _TipoPersonas;
+
+        public string Apellido
+        {
+            get { return this._Apellido; }
+            set { this._Apellido = value; }
+        }
+
+        public string Direccion
+        {
+            get { return this._Direccion; }
+            set { this._Direccion = value; }
+        }
+    }
+}
