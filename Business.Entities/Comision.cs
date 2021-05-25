188aeaaf3fe0befb83cd033a7fb93b6e2c934310
Ivan Biscaldi <ivanbisc12@gmail.com>
2021-05-25 03:18:09 -0300

    Parte 2 Lab 2 Pendiente

---
+++ Business.Entities/Comision.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Comision : BusinessEntity
+    {
+        private int _AnioEspecialidad, _IDPlan;
+        private string _Descripcion;
+
+        public int AnioEspecialidad
+        {
+            get { return this._AnioEspecialidad}
+            set { this._AnioEspecialidad = value; }
+        }
+
+        public int IDPlan
+        {
+            get { return this._IDPlan; }
+            set { this._IDPlan = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return this._Descripcion; }
+            set { this._Descripcion = value; }
+        }
+    }
+}
---
+++ Business.Entities/Usuario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Usuario : BusinessEntity
+    {
+        private string _NombreUsuario, _Clave, _Nombre, _Apellido, _Email;
+        private bool _Habilitado;
+
+        public string NombreUsuario
+        {
+            get { return this._NombreUsuario; }
+            set { this._NombreUsuario = value; }
+        }
+
+        public string Clave
+        {
+            get { return this._Clave; }
+            set { this._Clave = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this._Nombre; }
+            set { this._Nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return this._Apellido; }
+            set { this._Apellido = value; }
+        }
+
+        public string Email
+        {
+            get { return this._Email; }
+            set { this._Email = value; }
+        }
+
+        public bool Habilitado
+        {
+            get { return this._Habilitado; }
+            set { this._Habilitado = value; }
+        }
+    }
+}
