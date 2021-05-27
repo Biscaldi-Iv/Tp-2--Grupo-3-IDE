059ab377d869d2e13b8a93cbf0960216daca6653
Ivan Biscaldi <ibiscaldi@frro.utn.edu.ar>
2021-05-27 13:22:19 -0300

    Punto 3.3 Lab 3

--- Business.Logic/UsuarioLogic.cs
+++ Business.Logic/UsuarioLogic.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
 
 namespace Business.Logic
 {
-    public class UsuarioLogic: BusinessLogic
+    public class UsuarioLogic : BusinessLogic
     {
+        public UsuarioLogic()
+            {
+                
+            }
+
+        private Data.Database.UsuarioAdapter _UsuarioData;
+        public Data.Database.UsuarioAdapter UsuarioData
+        {
+           // get {; }
+            set {; }
+        }
     }
 }
