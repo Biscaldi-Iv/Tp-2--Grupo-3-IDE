2a69a9d9b253c3a8edebb83f3bc5dadc038741c2
Ivan Biscaldi <ibiscaldi@frro.utn.edu.ar>
2021-06-14 02:45:44 -0300

    Lab03 parte 3 completa

--- Business.Logic/UsuarioLogic.cs
+++ Business.Logic/UsuarioLogic.cs
@@ -10,16 +10,39 @@ namespace Business.Logic
 {
     public class UsuarioLogic : BusinessLogic
     {
-        public UsuarioLogic()
+        public UsuarioLogic(UsuarioAdapter userdata)
             {
-                
+            this.UsuarioData = userdata;
             }
 
         private Data.Database.UsuarioAdapter _UsuarioData;
         public Data.Database.UsuarioAdapter UsuarioData
         {
-           // get {; }
-            set {; }
+            get {return this._UsuarioData; }
+            set { this._UsuarioData = value; }
         }
+
+        //Crear los métodos GetOne, GetAll, Save y Delete en UsuarioLogic.
+        
+        public List<Usuario> GetAll() 
+        {
+            return this.UsuarioData.GetAll();
+        }
+
+        public Usuario GetOne(int id)
+        {
+            return this.GetOne(id);
+        }
+
+        public void Delete(int id)
+        {
+            this.UsuarioData.Delete(id);
+        }
+
+        public void Save(Usuario usr)
+        {
+            this.UsuarioData.Save(usr);
+        }
+
     }
 }
--- Data.DataBase/obj/Debug/net5.0/Data.DataBase.AssemblyInfo.cs
+++ Data.DataBase/obj/Debug/net5.0/Data.DataBase.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Data.DataBase")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Data.Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Data.DataBase")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Data.DataBase")]
+[assembly: System.Reflection.AssemblyProductAttribute("Data.Database")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Data.Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
