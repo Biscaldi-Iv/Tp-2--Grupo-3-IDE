3bc63f9a64c0eaa7e23bcd8d29cac4ef872d84e8
Cristian Gerster <cristian_gerster@hotmail.com>
2021-07-06 12:23:18 -0300

    subMenuABMC CG

---
+++ UI.Desktop/SubMenuABMC.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace UI.Desktop
+{
+    partial class SubMenuABMC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.btnEspecialidades = new System.Windows.Forms.Button();
+            this.btnComisiones = new System.Windows.Forms.Button();
+            this.btnProfesores = new System.Windows.Forms.Button();
+            this.btnAlumnos = new System.Windows.Forms.Button();
+            this.btnPlanesyMaterias = new System.Windows.Forms.Button();
+            this.btnCursos = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lABMC = new System.Windows.Forms.Label();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.tableLayoutPanel1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(800, 425);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(800, 450);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.btnUsuarios, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnEspecialidades, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnComisiones, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnProfesores, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnAlumnos, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnPlanesyMaterias, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnCursos, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnSalir, 4, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lABMC, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 425);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnUsuarios.Location = new System.Drawing.Point(197, 112);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(120, 30);
+            this.btnUsuarios.TabIndex = 0;
+            this.btnUsuarios.Text = "Usuarios";
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // btnEspecialidades
+            // 
+            this.btnEspecialidades.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnEspecialidades.Location = new System.Drawing.Point(197, 197);
+            this.btnEspecialidades.Name = "btnEspecialidades";
+            this.btnEspecialidades.Size = new System.Drawing.Size(120, 30);
+            this.btnEspecialidades.TabIndex = 1;
+            this.btnEspecialidades.Text = "Especialidades";
+            this.btnEspecialidades.UseVisualStyleBackColor = true;
+            // 
+            // btnComisiones
+            // 
+            this.btnComisiones.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnComisiones.Location = new System.Drawing.Point(197, 282);
+            this.btnComisiones.Name = "btnComisiones";
+            this.btnComisiones.Size = new System.Drawing.Size(120, 30);
+            this.btnComisiones.TabIndex = 2;
+            this.btnComisiones.Text = "Comisiones";
+            this.btnComisiones.UseVisualStyleBackColor = true;
+            // 
+            // btnProfesores
+            // 
+            this.btnProfesores.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnProfesores.Location = new System.Drawing.Point(340, 112);
+            this.btnProfesores.Name = "btnProfesores";
+            this.btnProfesores.Size = new System.Drawing.Size(120, 30);
+            this.btnProfesores.TabIndex = 3;
+            this.btnProfesores.Text = "Profesores";
+            this.btnProfesores.UseVisualStyleBackColor = true;
+            // 
+            // btnAlumnos
+            // 
+            this.btnAlumnos.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnAlumnos.Location = new System.Drawing.Point(483, 112);
+            this.btnAlumnos.Name = "btnAlumnos";
+            this.btnAlumnos.Size = new System.Drawing.Size(120, 30);
+            this.btnAlumnos.TabIndex = 4;
+            this.btnAlumnos.Text = "Alumnos";
+            this.btnAlumnos.UseVisualStyleBackColor = true;
+            // 
+            // btnPlanesyMaterias
+            // 
+            this.btnPlanesyMaterias.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnPlanesyMaterias.Location = new System.Drawing.Point(483, 197);
+            this.btnPlanesyMaterias.Name = "btnPlanesyMaterias";
+            this.btnPlanesyMaterias.Size = new System.Drawing.Size(120, 30);
+            this.btnPlanesyMaterias.TabIndex = 5;
+            this.btnPlanesyMaterias.Text = "Planes y Materias";
+            this.btnPlanesyMaterias.UseVisualStyleBackColor = true;
+            // 
+            // btnCursos
+            // 
+            this.btnCursos.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnCursos.Location = new System.Drawing.Point(483, 282);
+            this.btnCursos.Name = "btnCursos";
+            this.btnCursos.Size = new System.Drawing.Size(120, 30);
+            this.btnCursos.TabIndex = 6;
+            this.btnCursos.Text = "Cursos";
+            this.btnCursos.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSalir.Location = new System.Drawing.Point(660, 367);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(120, 30);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // lABMC
+            // 
+            this.lABMC.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lABMC.AutoSize = true;
+            this.lABMC.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lABMC.Location = new System.Drawing.Point(345, 24);
+            this.lABMC.Name = "lABMC";
+            this.lABMC.Size = new System.Drawing.Size(110, 37);
+            this.lABMC.TabIndex = 8;
+            this.lABMC.Text = "AMBC\'S";
+            // 
+            // SubMenuABMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "SubMenuABMC";
+            this.Text = "SubMenuABMC";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Button btnEspecialidades;
+        private System.Windows.Forms.Button btnComisiones;
+        private System.Windows.Forms.Button btnProfesores;
+        private System.Windows.Forms.Button btnAlumnos;
+        private System.Windows.Forms.Button btnPlanesyMaterias;
+        private System.Windows.Forms.Button btnCursos;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lABMC;
+    }
+}
