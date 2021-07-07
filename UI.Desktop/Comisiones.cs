3eabb9bf97e9579e5fd690449c45a7a8c88ae788
Sebastian <seba9988@hotmail.com>
2021-07-07 19:09:49 -0300

    comisiones form

---
+++ UI.Desktop/Comisiones.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Logic;
+using Business.Entities;
+
+namespace UI.Desktop
+{
+    public partial class Comisiones : Form
+    {
+        public Comisiones()
+        {
+            InitializeComponent();
+        }
+        public void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.dgvComisiones.DataSource = ul.GetAll();
+        }
+
+        private void Comisiones_Load(object sender, EventArgs e)
+        {
+            Listar();// paso 31
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
