92a480352189c12c493e5fc682bfabed3e60bf1e
Sebastian <seba9988@hotmail.com>
2021-07-08 06:55:32 -0300

    TipoPersonaLogic

--- Business.Logic/TipoPersonaLogic.cs
+++ Business.Logic/TipoPersonaLogic.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
 
 namespace Business.Logic
 {
-    class TipoPersonaLogic
+    class TipoPersonaLogic : BusinessLogic
     {
+        private TipoPersonaAdapter _TipoPersona_data;
+
+        public TipoPersonaAdapter TipoPersona_data
+        {
+            get => this._TipoPersona_data;
+            set => this._TipoPersona_data = value;
+        }
+        public TipoPersonas GetOne(int id)
+        {
+            try
+            {
+                return TipoPersona_data.GetOne(id);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+        }
+        public List<TipoPersonas> GetAll()
+        {
+            try
+            {
+                return TipoPersona_data.GetAll();
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+        }
+        public TipoPersonas GetByDesc(string des)
+        {
+            try
+            {
+                return TipoPersona_data.GetByDesc(des);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+        }
     }
 }
