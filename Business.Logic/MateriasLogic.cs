02211d63fb7b28bd2226f852cb040bd4a76a1bb0
Cristian Gerster <cristian_gerster@hotmail.com>
2021-08-12 11:55:01 -0300

    MateriasLogic

---
+++ Business.Logic/MateriasLogic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Database;
+using Business.Entities;
+
+namespace Business.Logic
+{
+    class MateriasLogic : BusinessLogic
+    {
+        public MateriasLogic()
+        {
+            this.Materia_data = new MateriasAdapter();
+        }
+
+        private MateriasAdapter _matAdpt;
+        public MateriasAdapter Materia_data
+        {
+            get { return this._matAdpt; }
+            set { this._matAdpt = value; }
+        }
+
+        public List<Materia> materias;
+        public List<Materia> Materias
+        {
+            get { return this.Materia_data.GetAll(); }
+        }
+
+        public List<Materia> GetAll()
+        {
+            return this.Materia_data.GetAll();
+        }
+        public Materia GetOne(int id)
+        {
+            return Materia_data.GetOne(id);
+        }
+
+        public void Delete(int id)
+        {
+            this.Materia_data.Delete(id);
+        }
+        public void Save(Materia mat)
+        {
+            this.Materia_data.SaveChanges(mat);
+        }
+        public void AddNew(Materia mat)
+        {
+            Materia_data.AddNew(mat);
+        }
+
+        public bool VerificarMateria(string desc)
+        {
+            try
+            {
+                var mat = from materia in this.Materias where materia.Descripcion == desc select materia;
+                if(mat.Any())
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
+            }
+            catch(Exception exc)
+            {
+                throw new Exception("Error en bd"); // errror sin manejar
+            }
+        }
+
+    }
+}
