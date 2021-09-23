34b58ab910398383c00c5e6299a7eeed31a3f7a7
Biscaldi-Iv <ivanbisc12@gmail.com>
2021-09-23 13:44:01 -0300

    web

---
+++ UI.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Web.App_Start
+{
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+        /*routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        routes.MapRoute(
+            name: "Student",
+            url: "students/{id}",
+            defaults: new { controller = "Student", action = "Index" }
+        );
+
+        routes.MapRoute(
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        );*/
+    }
+}
