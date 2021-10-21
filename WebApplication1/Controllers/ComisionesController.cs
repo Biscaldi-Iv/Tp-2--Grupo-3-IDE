f67b9068f238910105aafeceab3e06d83d175a27
Biscaldi-Iv <ivanbisc12@gmail.com>
2021-10-21 12:30:05 -0300

    Comision web

--- WebApplication1/Controllers/ComisionesController.cs
+++ WebApplication1/Controllers/ComisionesController.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Entities;
+using Business.Logic;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,44 @@ namespace UI.Web.Controllers
         {
             TipoPersonas tp = Models.SessionHepler.GetTipoPersona(HttpContext.Session);
             ViewBag.tipo = tp.Descripcion;
+            ComisionLogic cl = new ComisionLogic();
+            PlanesLogic pl = new PlanesLogic();
+            List<Plan> planes = pl.GetAll();
+            List<Comision> comisiones = cl.GetAll();
+            var coms = from comis in comisiones
+                       join pls in planes on comis.IDPlan equals pls.ID
+                       select
+            (comis.ID, comis.Descripcion, pls.Descripcion, comis.AnioEspecialidad, pls.ID);
+            ViewBag.comisiones = coms;
+            ViewBag.planes = planes;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Nuevo(string descCom1, int plan1, int anio1)
+        {
+            ComisionLogic cl = new ComisionLogic();
+            Comision c = new Comision(0, anio1, plan1, descCom1);
+            cl.AddNew(c);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult GuardarCambio(int idCom2,string descCom2, int plan2, int anio2)
+        {
+            //solucionar error de edicion porque no se selecciona plan
+            ComisionLogic cl = new ComisionLogic();
+            Comision c = new Comision(idCom2, anio2, plan2, descCom2);
+            cl.SaveChanges(c);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int idCom3)
+        {
+            ComisionLogic cl = new ComisionLogic();
+            cl.Delete(idCom3);
+            return RedirectToAction("Index");
         }
     }
 }
