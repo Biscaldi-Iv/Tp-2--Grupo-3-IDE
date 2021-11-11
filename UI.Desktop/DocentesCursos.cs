cf5e1eeedaff6580e3931856734524453971170a
Cristian Gerster <cristian_gerster@hotmail.com>
2021-11-10 23:22:33 -0300

    color docentecurso

--- UI.Desktop/DocentesCursos.cs
+++ UI.Desktop/DocentesCursos.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace UI.Desktop
 {
@@ -16,8 +17,34 @@ namespace UI.Desktop
     {                
         public DocentesCursos()
         {
-            InitializeComponent();                       
+            InitializeComponent();
+            LoadTheme();
+            UserPreferenceChanged = new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
+            SystemEvents.UserPreferenceChanged += UserPreferenceChanged;
+            this.Disposed += new EventHandler(Form_Disposed);
         }
+        private UserPreferenceChangedEventHandler UserPreferenceChanged;
+
+        private void Form_Disposed(object sender, EventArgs e)
+        {
+            SystemEvents.UserPreferenceChanged -= UserPreferenceChanged;
+        }
+
+        private void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            if (e.Category == UserPreferenceCategory.General || e.Category == UserPreferenceCategory.VisualStyle)
+            {
+                LoadTheme();
+            }
+        }
+
+        private void LoadTheme()
+        {
+            var themeColor = WinTema.GetAccentColor();
+            tscDocentesCursos.TopToolStripPanel.BackColor = themeColor;
+            tsDocentesCursos.BackColor = themeColor;
+        }
+
         public void Listar()
         {
             CursosLogic curso = new CursosLogic();
